eafc6142cb303326023f5310dd066dc44ef7bc6a
Felipe Armando <felipearmando2107@gmail.com>
2022-07-11 18:00:53 -0300

    Auditing data

--- backend/src/main/resources/import.sql
+++ backend/src/main/resources/import.sql
@@ -1,1 +1,1 @@
-INSERT INTO tb_category (name) VALUES ('Books'), ('Electronics'), ('Computers');
+INSERT INTO tb_category (name, created_At) VALUES ('Books', now()), ('Electronics', now()), ('Computers', now());
