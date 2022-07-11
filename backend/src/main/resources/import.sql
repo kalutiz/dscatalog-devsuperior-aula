15d73ddb8b6b5c96dd788d1c77c17592d344bff3
Felipe Armando <felipearmando2107@gmail.com>
2022-07-11 18:54:23 -0300

    Pagination

--- backend/src/main/resources/import.sql
+++ backend/src/main/resources/import.sql
@@ -1,1 +1,3 @@
-INSERT INTO tb_category (name, created_At) VALUES ('Books', now()), ('Electronics', now()), ('Computers', now());
+INSERT INTO tb_category (name, created_At) VALUES ('Books', now());
+INSERT INTO tb_category (name, created_At) VALUES ('Eletronics', now());
+INSERT INTO tb_category (name, created_At) VALUES ('Computers', now());
