02f8d9e73c169d697511bfb40984695c1cb1dbb8
Felipe Armando <felipearmando2107@gmail.com>
2022-07-12 18:02:29 -0300

    att projeto

--- backend/src/main/java/com/devsuperior/dscatalog/dto/CategoryDTO.java
+++ backend/src/main/java/com/devsuperior/dscatalog/dto/CategoryDTO.java
@@ -5,37 +5,37 @@ import com.devsuperior.dscatalog.entities.Category;
 import java.io.Serializable;
 
 public class CategoryDTO implements Serializable {
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
-	private Long id;
-	private String name;
-	
-	public CategoryDTO() {
-	}
+    private Long id;
+    private String name;
 
-	public CategoryDTO(Long id, String name) {
-		this.id = id;
-		this.name = name;
-	}
-	
-	public CategoryDTO(Category entity) {
-		this.id = entity.getId();
-		this.name = entity.getName();
-	}
+    public CategoryDTO() {
+    }
 
-	public Long getId() {
-		return id;
-	}
+    public CategoryDTO(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
 
-	public void setId(Long id) {
-		this.id = id;
-	}
+    public CategoryDTO(Category entity) {
+        this.id = entity.getId();
+        this.name = entity.getName();
+    }
 
-	public String getName() {
-		return name;
-	}
+    public Long getId() {
+        return id;
+    }
 
-	public void setName(String name) {
-		this.name = name;
-	}
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
