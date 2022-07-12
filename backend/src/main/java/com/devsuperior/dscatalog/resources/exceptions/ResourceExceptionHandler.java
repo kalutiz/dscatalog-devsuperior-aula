7e676cf5e52e18bd11e2e715ce92b9b765bfdd70
Felipe Armando <felipearmando2107@gmail.com>
2022-07-12 18:00:08 -0300

    att projeto

--- backend/src/main/java/com/devsuperior/dscatalog/resources/exceptions/ResourceExceptionHandler.java
+++ backend/src/main/java/com/devsuperior/dscatalog/resources/exceptions/ResourceExceptionHandler.java
@@ -1,16 +1,14 @@
 package com.devsuperior.dscatalog.resources.exceptions;
 
-import java.time.Instant;
-
-import javax.servlet.http.HttpServletRequest;
-
+import com.devsuperior.dscatalog.services.exceptions.DataBaseException;
+import com.devsuperior.dscatalog.services.exceptions.ResourceNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
-import com.devsuperior.dscatalog.services.exceptions.DataBaseException;
-import com.devsuperior.dscatalog.services.exceptions.ResourceNotFoundException;
+import javax.servlet.http.HttpServletRequest;
+import java.time.Instant;
 
 @ControllerAdvice
 public class ResourceExceptionHandler {
