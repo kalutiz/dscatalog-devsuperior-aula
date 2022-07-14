1220dffa37aecf43cec15bdb00b09e8bad0ab204
Felipe Armando <felipearmando2107@gmail.com>
2022-07-14 18:52:37 -0300

    Segundo teste JUnit

--- aula-junit/src/main/java/tests/entities/AccountTests.java
+++ aula-junit/src/main/java/tests/entities/AccountTests.java
@@ -6,11 +6,23 @@ import org.junit.jupiter.api.Test;
 
 public class AccountTests {
     @Test
-    public void depositShouldIncreaseBalanceWhenPositiveAmount() {
+    public void depositShouldIncreaseBalanceAndDiscountFeedWhenPositiveAmount() {
 
         double amount = 200.0;
         double expectedValue = 196.0;
         Account acc = new Account(1L, 0.0);
+
+        acc.deposit(amount);
+
+        Assertions.assertEquals(expectedValue, acc.getBalance());
+    }
+
+    @Test
+    public void depositShouldDoNothingWhenNegativeAmount() {
+
+        double expectedValue = 100.0;
+        Account acc = new Account(1L, expectedValue);
+        double amount = -200.0;
 
         acc.deposit(amount);
 
